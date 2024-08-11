728c2c7d4a7812f72bbdc51766c0e500f118ec0b
ocean <1095707172@qq.com>
2024-08-11 16:29:57 +0800

    :apple: button的icon，加载动画

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,23 +1,42 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { ButtonProps } from './types';
+import { ref, computed } from 'vue';
+import type { ButtonProps, ButtonEmits, ButtonInstance } from "./types";
+import { throttle } from "lodash-es";
+import ErIcon from '../Icon/Icon.vue';
+
 defineOptions({
     name: 'ErButton'
 })
 const props = withDefaults(defineProps<ButtonProps>(),{
     tag:"button",
-    nativeType:"button"
+    nativeType:"button",
+    useThrottle: true,
+    throttleDuration: 500,
 })
+
+const emits = defineEmits<ButtonEmits>();
+
 const slots = defineSlots()
 
 const _ref = ref<HTMLButtonElement>()
+const iconStyle = computed(()=>({marginRight: slots.default ? "6px" : "0px"}))
+
+const handleBtnClick = (e: MouseEvent) => {
+  emits("click", e);
+};
+const handlBtneCLickThrottle = throttle(handleBtnClick, props.throttleDuration);
+
+defineExpose<ButtonInstance>({
+    ref: _ref,
+});
 </script>
 
 <template>
     <component
-        :is="props.tag"
         ref = "_ref"
         class = "er-button"
+        :is="tag"
+        :autofocus="autofocus"
         :type="tag==='button'?nativeType: void 0"
         :disabled="disabled || loading ? true: void 0"
         :class = "{
@@ -29,7 +48,32 @@ const _ref = ref<HTMLButtonElement>()
             'is-disabled': disabled,
             'is-loading': loading,
         }"
+        @click="
+        (e: MouseEvent) =>
+            useThrottle ? handlBtneCLickThrottle(e) : handleBtnClick(e)
+        "
     >
-        <slot></slot>
+    <template v-if="loading">
+        <slot name="loading">
+            <er-icon
+                class="loading-icon"
+                :icon="loadingIcon || 'spinner'"
+                :style="iconStyle"
+                size="1x"
+                spin
+            />
+        </slot> 
+    </template>
+    <er-icon
+      :icon="icon"
+      size="1x"
+      :style="iconStyle"
+      v-if="icon && !loading"
+    />
+    <slot></slot>
     </component>
 </template>
+
+<style scoped>
+@import './style.css';
+</style>
